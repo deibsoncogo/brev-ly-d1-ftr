ecbf117a1734517f9e54d60e1fd2e129a31349aa
Deibson Cogo <deibsoncogo@outlook.com>
2025-06-13 21:33:30 -0300

    feat: implementação visual da página de redirecionamento dos links curtos

--- web/src/pages/short-link.tsx
+++ web/src/pages/short-link.tsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom"
+
 export function ShortLink() {
-  return <h1>Short Link</h1>
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <div className="py-16 rounded-lg bg-gray-100 flex flex-col justify-center items-center max-w-[580px] p-5 m-5 gap-6">
+        <img src="/src/assets/icon.svg" alt="404" className="w-11" />
+
+        <h2 className="text-gray-600 text-2xl font-bold leading-8">Redirecionando...</h2>
+
+        <p className="text-center text-gray-500 text-sm font-semibold leading-5">
+          O link será aberto automaticamente em alguns instantes. Não foi redirecionado?{" "}
+          <Link to="/" className="text-blue-base underline">
+            Acesse aqui
+          </Link>
+          .
+        </p>
+      </div>
+    </main>
+  )
 }

301609eb6a2eeb14498e941f5c99ba08f4d436a7
Deibson Cogo <deibsoncogo@outlook.com>
2025-06-19 10:06:09 -0300

    feat: criação do componente link

---
+++ web/src/components/link-component.tsx
@@ -0,0 +1,64 @@
+import type { Link } from "../interfaces/link"
+
+interface Props {
+  link: Link
+  deleteLink: (id: string) => Promise<void>
+}
+
+export function LinkComponent({
+  link: { id, originalLink, shortLink, accesses },
+  deleteLink,
+}: Props) {
+  const fullShortLink = `${import.meta.env.VITE_BASE_URL_FRONTEND}/${shortLink}`
+
+  function handlerCopyShortLink(): void {
+    navigator.clipboard.writeText(fullShortLink)
+  }
+
+  async function handlerDeleteLink(): Promise<void> {
+    await deleteLink(id)
+  }
+
+  return (
+    <div className="flex justify-between items-center py-4 last:pb-0 gap-3 border-t border-gray-200">
+      <div className="flex flex-col justify-center gap-1">
+        <strong className="text-sm leading-4 font-semibold text-blue-base">
+          {fullShortLink}
+        </strong>
+
+        <a
+          href={originalLink}
+          target="_blank"
+          rel="noreferrer"
+          className="text-xs leading-3.5 text-gray-500 hover:underline outline-none focus:underline"
+        >
+          {originalLink}
+        </a>
+      </div>
+
+      <div className="flex items-center gap-5">
+        <p className="text-xs leading-3.5 text-gray-500">
+          {accesses} acesso{accesses > 1 && "s"}
+        </p>
+
+        <div className="flex gap-1">
+          <button
+            type="button"
+            onClick={handlerCopyShortLink}
+            className="flex justify-center items-center size-8 bg-gray-200 rounded-sm cursor-pointer outline-blue-base hover:bg-gray-300"
+          >
+            <img src="/src/assets/copy.svg" alt="copiar" className="size-3" />
+          </button>
+
+          <button
+            type="button"
+            onClick={handlerDeleteLink}
+            className="flex justify-center items-center size-8 bg-gray-200 rounded-sm cursor-pointer outline-blue-base hover:bg-gray-300"
+          >
+            <img src="/src/assets/bin.svg" alt="excluir" className="size-3" />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ web/src/interfaces/link.ts
@@ -0,0 +1,7 @@
+export interface Link {
+  id: string
+  originalLink: string
+  shortLink: string
+  accesses: number
+  createdAt: string
+}

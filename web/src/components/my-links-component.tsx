89d6608f01d6795ffd1e6269a3c84fb91ae688f7
Deibson Cogo <deibsoncogo@outlook.com>
2025-06-24 23:00:00 -0300

    feat: criação do carregamento enquanto exporta os links

--- web/src/components/my-links-component.tsx
+++ web/src/components/my-links-component.tsx
@@ -9,19 +9,23 @@ import { TopLoadingBarComponent } from "./top-loading-bar-component"
 import { ButtonUi } from "./ui/button-ui"
 
 export function MyLinksComponent() {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState({ listSort: true, exportLinks: false })
   const { links, setLinks } = useLinkStore()
 
   async function handleExportLinks() {
+    setIsLoading(prev => ({ ...prev, exportLinks: true }))
+
     await api
       .post("/links/csv")
       .then(response => {
-        window.open(response.data.reportUrl, "_blank")
+        window.open(response.data.reportUrl, "_self")
         toast.success("Exportação executado com sucesso!")
       })
       .catch(() => {
         toast.error("Falha ao exportar os dados!")
       })
+
+    setIsLoading(prev => ({ ...prev, exportLinks: false }))
   }
 
   useEffect(() => {
@@ -34,13 +38,13 @@ export function MyLinksComponent() {
         toast.error("Falha ao listar os links!")
       })
       .finally(() => {
-        setIsLoading(false)
+        setIsLoading(prev => ({ ...prev, listSort: false }))
       })
   }, [setLinks])
 
   return (
     <section className="relative flex-1 self-start w-full p-8 bg-gray-100 rounded-lg overflow-hidden md:max-w-[580px]">
-      {isLoading && <TopLoadingBarComponent />}
+      {isLoading.listSort && <TopLoadingBarComponent />}
 
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-gray-600 font-bold text-lg leading-6">Meus links</h1>
@@ -49,15 +53,19 @@ export function MyLinksComponent() {
           type="button"
           size="rectangular"
           onClick={handleExportLinks}
-          disabled={links?.length === 0}
+          disabled={links?.length === 0 || isLoading.listSort || isLoading.exportLinks}
         >
-          <img src="/src/assets/download.svg" alt="download" className="size-3" />
+          {isLoading.exportLinks ? (
+            <img src="/src/assets/spinner.svg" alt="download" className="size-3" />
+          ) : (
+            <img src="/src/assets/download.svg" alt="download" className="size-3" />
+          )}
           Baixar CSV
         </ButtonUi>
       </div>
 
       <div className="max-h-[35lvh] md:max-h-[65lvh] overflow-y-auto">
-        {isLoading ? (
+        {isLoading.listSort ? (
           <LoadingLinksComponent />
         ) : links?.length === 0 ? (
           <NoLinkComponent />
